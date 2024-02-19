56c6ee501b32e07314dc30c39768af403055d47c
Quarkubit <AbramovYD+github@gmail.com>
2024-02-19 15:49:08 +0300

    gcd

--- exampls/Fraction.cpp
+++ exampls/Fraction.cpp
@@ -62,7 +62,39 @@ public:
         return numerator * F.denominator != denominator * F.numerator;
     }
 
+    // Алгоритм Евклида
+    int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
     // бинарные +, -, <, >
+    Fraction operator+(Fraction F)
+    {
+        Fraction res;
+        res.numerator = numerator * F.denominator + F.numerator * denominator;
+        res.denominator = denominator * F.denominator;
+        return res;
+    }
+
+    Fraction operator+(int F)
+    {
+        Fraction res;
+        res.numerator = numerator + F * denominator;
+        res.denominator = denominator;
+        return res;
+    }
+
+    Fraction operator-(Fraction F)
+    {
+        Fraction res;
+        res.numerator = numerator * F.denominator - F.numerator * denominator;
+        res.denominator = denominator * F.denominator;
+        return res;
+    }
+    //
 
     Fraction operator-()
     {
@@ -72,14 +104,14 @@ public:
         return res;
     }
 
-    friend Fraction operator*(int a,Fraction F);
+    friend Fraction operator*(int a, Fraction F);
 };
 
-Fraction operator*(int a,Fraction F)
+Fraction operator*(int a, Fraction F)
 {
     Fraction res;
-    res.numerator=a*F.numerator;
-    res.denominator=F.denominator;
+    res.numerator = a * F.numerator;
+    res.denominator = F.denominator;
     return res;
 }
 
@@ -88,14 +120,17 @@ int main()
     Fraction F(1, 2);
     Fraction G(3, 4);
     Fraction H = F / G * 2;
-    Fraction K(2,2);
-    Fraction J = K/2; J.print();
-    Fraction D = 2*K; D.print();
-    cout<<"\nF*G = "; H.print();
-    cout<<"\nF==F "<<(F==F);
-    cout<<"\nF!=F "<<(F!=F);
-    cout<<"\n-F = "; (-F).print();
-
+    Fraction K(2, 2);
+    Fraction J = K / 2;
+    J.print();
+    Fraction D = 2 * K;
+    D.print();
+    cout << "\nF*G = ";
+    H.print();
+    cout << "\nF==F " << (F == F);
+    cout << "\nF!=F " << (F != F);
+    cout << "\n-F = ";
+    (-F).print();
 
     return 0;
 }
